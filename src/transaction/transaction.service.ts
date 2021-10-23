2f54e8d1fb738e4c6fe63e2ba727664aee5f6b62
Júlio Sandron <sandronister@icloud.com>
2021-10-23 19:36:01 -0300

    remove onModuleInit interface

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TransactionEntity } from 'src/entity';
 import { Repository } from 'typeorm';
